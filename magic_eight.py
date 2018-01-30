925cb158519e0e49eecdaa26c737a5fdca458d62
Wen Hoong <lingwenhoong@gmail.com>
2018-01-30 15:12:49 -0500

    Cleaned up clutter

--- magic_eight.py
+++ magic_eight.py
@@ -4,15 +4,13 @@ def question():
     s = input("What is your question?")
     return(s)
 
-<<<<<<< HEAD
 ans = ["It is certain","It is decidedly so","Without a doubt","Yes definitely","You may rely on it","As I see it, yes","Most likely",
 "Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now",
 "Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]
 
 def answers():
     return random.choice(ans)
->>>>>>> add_questions
-=======
+
 
 def check_question(s):
     if s[-1] == "?":
@@ -22,4 +20,3 @@ def check_question(s):
         questions()
 
 question()
->>>>>>> fec9b8823a65f920ff0b58026df592ace95b99af
